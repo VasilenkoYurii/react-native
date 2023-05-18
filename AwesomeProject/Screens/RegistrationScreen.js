8f7c1de4b9fd2434ab4eb45f6ad552e641bbf254
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-05-18 21:52:47 +0300

    add many styles

--- AwesomeProject/Screens/RegistrationScreen.js
+++ AwesomeProject/Screens/RegistrationScreen.js
@@ -1,18 +1,43 @@
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
 
 export const RegistrationScreen = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <ImageBackground
         source={require("../images/regLogBg.png")}
         style={styles.bgImage}
       >
         <View style={styles.formContainer}>
-          <Text style={styles.formText}>Hello world</Text>
-          <Text>First react native app</Text>
+          <Text style={styles.formTitle}>Регистрация</Text>
+          <TextInput style={styles.formInput} placeholder="Логин" />
+          <TextInput
+            style={styles.formInput}
+            placeholder="Адрес электронной почты"
+          />
+          <View style={styles.passwordcontainer}>
+            <TextInput
+              style={styles.formLastInput}
+              placeholder="Пароль"
+              type="password"
+            />
+            <Text style={styles.showPassword}>Показать</Text>
+          </View>
+          <Button
+            title="Зарегистрироваться"
+            color="#FF6C00"
+            style={styles.formButtomSubmit}
+          />
         </View>
       </ImageBackground>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -22,16 +47,80 @@ const styles = StyleSheet.create({
   },
   bgImage: {
     flex: 1,
-    // justifyContent: "center",
-    width: 414.4,
+    justifyContent: "flex-end",
+    width: "100%",
     height: 812,
   },
   formContainer: {
     backgroundColor: "#ffffff",
-    width: 400,
-    height: 400,
+    width: "100%",
+    height: 549,
+    paddingRight: 16,
+    paddingLeft: 16,
+    borderTopEndRadius: 25,
+    borderTopStartRadius: 25,
+
+    alignItems: "center",
   },
   formText: {
     backgroundColor: "#fff",
+  },
+  formTitle: {
+    // fontFamily: "Roboto",
+    fontStyle: "Medium",
+    color: "#212121",
+    marginTop: 92,
+    marginBottom: 33,
+    fontSize: 30,
+    lineHeight: 35.16,
+  },
+
+  formInput: {
+    marginBottom: 16,
+    padding: 16,
+
+    backgroundColor: "#f6f6f6",
+    borderColor: "#E8E8E8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 50,
+    width: "100%",
+
+    fontSize: 16,
+    lineHeight: 18.75,
+  },
+  passwordcontainer: {
+    position: "relative",
+    width: "100%",
+  },
+  showPassword: {
+    position: "absolute",
+    top: 16,
+    right: 16,
+    color: "#1B4371",
+    fontSize: 16,
+    lineHeight: 18.75,
+  },
+  formLastInput: {
+    marginBottom: 43,
+
+    padding: 16,
+
+    backgroundColor: "#f6f6f6",
+    borderColor: "#E8E8E8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 50,
+    width: "100%",
+
+    fontSize: 16,
+    lineHeight: 18.75,
+  },
+
+  formButtomSubmit: {
+    width: "100%",
+    borderRadius: 100,
   },
 });
